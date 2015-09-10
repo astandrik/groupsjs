bec5890c337cf6036c43bca0cd096a754eb1ef47
Anton <astandrik@gmail.com>
2015-09-11 00:15:22 +0300

    make Closure

--- groups.js
+++ groups.js
@@ -21,3 +21,25 @@ function isPermInGroup(group, perm) {
   });
   return result;
 }
+
+function makeClosure(initGroup) {
+  var flag = true;
+  var newGroup = [];
+  for(var i = 0; i < initGroup.length; i++) {
+    for(var j = 0; j < initGroup.length; j++) {
+      var newPermRight = multiPerm(initGroup[i], initGroup[j]);
+      var newPermLeft = multiPerm(initGroup[j], initGroup[i]);
+      if(!isPermInGroup(initGroup, newPermRight)) {
+        initGroup.push(newPermRight);
+        i = 0;
+        j = 0;
+      }
+      if(!isPermInGroup(initGroup, newPermLeft)) {
+        initGroup.push(newPermLeft);
+        i = 0;
+        j = 0;
+      }
+    }
+  }
+  return initGroup;
+}
